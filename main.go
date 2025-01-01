369ca30a65f72dc0205275a2f2041629bb034120
Mysh! <me@mysh.dev>
2025-01-01 16:51:22 +0000

    feat: when running forum first time, create some example posts and such

--- main.go
+++ main.go
@@ -35,11 +35,58 @@ func main() {
 	}
 
 	if !adminExists {
-		if _, err := data.CreateUser("admin", model.UserCreationRequest{
+		userID, err := data.CreateUser("admin", model.UserCreationRequest{
 			Name:     "admin",
 			Password: "admin",
 			IsAdmin:  true,
-		}); err != nil {
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		forumID, err := data.CreateForum(model.ForumRequest{
+			Name:     "Getting started",
+			Position: 0,
+			IsLocked: false,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		boardID, err := data.CreateBoard(model.BoardRequest{
+			Name:        "Your first board",
+			Description: "This is an example board, you can check out what articles it haves.",
+			Position:    0,
+			IsLocked:    false,
+			ForumId:     forumID,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = data.CreateTopic(userID, model.TopicRequest{
+			Subject: "Change your admin account password NOW",
+			Content: `Perhaps it would be a good idea to change your default admin password!
+			
+## Changing the password.
+			
+Navigate to [/admin/users](/admin/users) route. There, you should be able to find the **admin** user and set a new password to whatever you prefer.`,
+			BoardId:  boardID,
+			IsLocked: true,
+			IsSticky: true,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = data.CreateTopic(userID, model.TopicRequest{
+			Subject:  "What to do?",
+			Content:  `Navigate to [/admin](/admin) route and see what you can poke in there. Just make sure to not delete yourself, okay? You will be locked in weird state otherwise.`,
+			BoardId:  boardID,
+			IsLocked: false,
+			IsSticky: false,
+		})
+		if err != nil {
 			log.Fatal(err)
 		}
 	}
