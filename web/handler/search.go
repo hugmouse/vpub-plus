f286c31ee2e5c7a4b54f45c1c26fd13a04f738e4
Mysh <me@mysh.dev>
2025-03-02 15:38:05 +0100

    fix(search): return server error if something happens with search query

--- web/handler/search.go
+++ web/handler/search.go
@@ -6,7 +6,11 @@ import (
 
 func (h *Handler) searchShow(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query().Get("q")
-	thing, _ := h.storage.Search(query)
+	thing, err := h.storage.Search(query)
+	if err != nil {
+		serverError(w, err)
+		return
+	}
 	v := NewView(w, r, "search")
 	v.Set("sql", thing)
 	v.Set("q", query)
