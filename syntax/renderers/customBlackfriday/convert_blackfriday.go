4a378848961e5dc042ef767bfd01fe5e2dc46899
hugmouse <me@mysh.dev>
2025-06-06 22:10:07 +0200

    refactor(blackfriday): add caching with TTL
    Benchmarking results:
    - Before: 3275 reqs/s
    - After : 4982 reqs/s

--- syntax/renderers/customBlackfriday/convert_blackfriday.go
+++ syntax/renderers/customBlackfriday/convert_blackfriday.go
@@ -9,14 +9,51 @@
 package customBlackfriday
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
 	"io"
 	"net/url"
 	"strings"
+	"sync"
+	"time"
 	"vpub/syntax"
 )
+
+const cacheTTL = 5 * time.Minute
+const cleanupInterval = 1 * time.Minute
+
+var rendererCache sync.Map
+
+type cacheEntry struct {
+	data      string
+	timestamp time.Time
+}
+
+func init() {
+	go startCacheCleanup()
+}
+
+func startCacheCleanup() {
+	ticker := time.NewTicker(cleanupInterval)
+	for range ticker.C {
+		cleanupCache()
+	}
+}
+
+func cleanupCache() {
+	now := time.Now()
+	rendererCache.Range(func(key, value any) bool {
+		if entry, ok := value.(cacheEntry); ok {
+			if now.Sub(entry.timestamp) > cacheTTL {
+				rendererCache.Delete(key)
+			}
+		}
+		return true
+	})
+}
 
 type BlackfridayRenderer struct{}
 
@@ -34,6 +71,21 @@ func (r *imageProxyRenderer) RenderNode(w io.Writer, node *blackfriday.Node, ent
 }
 
 func (b *BlackfridayRenderer) Convert(gmiContent string, wrap bool) string {
+	h := sha1.New()
+	if _, err := h.Write([]byte(gmiContent)); err != nil {
+		return fmt.Errorf("SHA-1 hash error: %w", err).Error()
+	}
+	sum := h.Sum(nil)
+
+	cacheKey := hex.EncodeToString(sum)
+
+	if cached, ok := rendererCache.Load(cacheKey); ok {
+		entry := cached.(cacheEntry)
+		if time.Since(entry.timestamp) < cacheTTL {
+			return entry.data // Cache hit!
+		}
+	}
+
 	input := strings.ReplaceAll(gmiContent, "\r\n", "\n")
 	params := blackfriday.HTMLRendererParameters{
 		Flags: blackfriday.CommonHTMLFlags,
@@ -45,8 +97,16 @@ func (b *BlackfridayRenderer) Convert(gmiContent string, wrap bool) string {
 		[]byte(input),
 		blackfriday.WithRenderer(proxy),
 		blackfriday.WithExtensions(blackfriday.CommonExtensions),
+		blackfriday.WithExtensions(blackfriday.HeadingIDs),
+		blackfriday.WithExtensions(blackfriday.Footnotes),
+		blackfriday.WithExtensions(blackfriday.Titleblock),
 	)
 	safe := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+
+	rendererCache.Store(cacheKey, cacheEntry{
+		data:      string(safe),
+		timestamp: time.Now(),
+	})
 	return string(safe)
 }
 
