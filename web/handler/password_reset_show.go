b3f5023b18b3c0629f13ac5503e529d8190c1584
hugmouse <me@mysh.dev>
2026-05-31 16:34:17 +0200

    fix(password reset): information disclosure

--- web/handler/password_reset_show.go
+++ web/handler/password_reset_show.go
@@ -9,16 +9,6 @@ func (h *Handler) showResetPasswordView(w http.ResponseWriter, r *http.Request) 
 	if val, ok := r.URL.Query()["hash"]; ok && len(val) == 1 {
 		hash = val[0]
 	}
-	userHashExists, err := h.storage.UserHashExists(hash)
-	if err != nil {
-		serverError(w, err)
-		return
-	}
-	if !userHashExists {
-		notFound(w)
-		return
-	}
-
 	v := NewView(w, r, "reset_password")
 	v.Set("hash", hash)
 	v.Render()
